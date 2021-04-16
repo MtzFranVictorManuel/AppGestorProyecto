c08ebec15ca76fbebd3c45e29f25d8d327c5839d
MtzFranVictorManuel <zs18019639@estudiantes.uv.mx>
2021-04-16 13:14:28 -0500

    Modificacion y agragacion
    Se modifico la clase Curriculum con los valores asignados en la base de datos y se agrego la clase integrante

--- src/domain/Curriculum.java
+++ src/domain/Curriculum.java
@@ -10,63 +10,23 @@ package domain;
  * @author azul_
  */
 public class Curriculum {
-    private String institucionDependecia;
-    private String facultad;
-    private String grado;
-    private String clave;
-    private int numeroIntegrantes;
-    private int numeroColaboladores;
-    private String fechaRegistro;
-    private String objetiviGeneral;
+    private String nombre;
     private String mision;
     private String vision;
+    private String objetivoGeneral;
 
     public Curriculum() {
     }
 
-    public Curriculum(String institucionDependecia, String facultad, String grado, String clave, int numeroIntegrantes, int numeroColaboladores, String fechaRegistro, String objetiviGeneral, String mision, String vision) {
-        this.institucionDependecia = institucionDependecia;
-        this.facultad = facultad;
-        this.grado = grado;
-        this.clave = clave;
-        this.numeroIntegrantes = numeroIntegrantes;
-        this.numeroColaboladores = numeroColaboladores;
-        this.fechaRegistro = fechaRegistro;
-        this.objetiviGeneral = objetiviGeneral;
+    public Curriculum(String nombre, String mision, String vision, String objetivoGeneral) {
+        this.nombre = nombre;
         this.mision = mision;
         this.vision = vision;
+        this.objetivoGeneral = objetivoGeneral;
     }
 
-    public String getInstitucionDependecia() {
-        return institucionDependecia;
-    }
-
-    public String getFacultad() {
-        return facultad;
-    }
-
-    public String getGrado() {
-        return grado;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public int getNumeroIntegrantes() {
-        return numeroIntegrantes;
-    }
-
-    public int getNumeroColaboladores() {
-        return numeroColaboladores;
-    }
-
-    public String getFechaRegistro() {
-        return fechaRegistro;
-    }
-
-    public String getObjetiviGeneral() {
-        return objetiviGeneral;
+    public String getNombre() {
+        return nombre;
     }
 
     public String getMision() {
@@ -77,36 +37,12 @@ public class Curriculum {
         return vision;
     }
 
-    public void setInstitucionDependecia(String institucionDependecia) {
-        this.institucionDependecia = institucionDependecia;
+    public String getObjetivoGeneral() {
+        return objetivoGeneral;
     }
 
-    public void setFacultad(String facultad) {
-        this.facultad = facultad;
-    }
-
-    public void setGrado(String grado) {
-        this.grado = grado;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
-    public void setNumeroIntegrantes(int numeroIntegrantes) {
-        this.numeroIntegrantes = numeroIntegrantes;
-    }
-
-    public void setNumeroColaboladores(int numeroColaboladores) {//hola actualiza
-        this.numeroColaboladores = numeroColaboladores;
-    }
-
-    public void setFechaRegistro(String fechaRegistro) {
-        this.fechaRegistro = fechaRegistro;
-    }
-
-    public void setObjetiviGeneral(String objetiviGeneral) {
-        this.objetiviGeneral = objetiviGeneral;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public void setMision(String mision) {
@@ -117,7 +53,8 @@ public class Curriculum {
         this.vision = vision;
     }
 
-
-    
+    public void setObjetivoGeneral(String objetivoGeneral) {
+        this.objetivoGeneral = objetivoGeneral;
+    }
     
 }
